c57e0fb93ab01ea22e6db4deb30a2ef6d3484a7f
Astha <asthabarnwal@gmail.com>
2023-07-09 20:37:00 +0530

    Final

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './index.css'
+import Banner from "./Components/Banner/Banner";
+import Exhibit from "./Components/Exhibit/Exhibit";
+import AddressDisplay from "./Components/AddressDisplay/AddressDisplay";
+import PylonConnect from "./Components/Connect/PylonConnect";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+                <Banner bannerText="Sextant" />
+                <Exhibit name="Public IPv4 Address">
+                    <AddressDisplay url='https://api.ipify.org?format=json' />
+                </Exhibit>
+                <Exhibit name="Public IPv6 Address">
+                    <AddressDisplay url='https://api64.ipify.org?format=json' />
+                </Exhibit>
+                <Exhibit name="Pylon Packet Latency">
+                    <PylonConnect />
+                </Exhibit>
+            </div>
   );
-}
+};
+
 
 export default App;
---
+++ sextant/src/Components/AddressDisplay/AddressDisplay.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react';
+
+const AddressDisplay = ({ url }) => {
+  const [ipAddress, setIPAddress] = useState(null);
+
+  useEffect(() => {
+    const fetchIPAddress = async () => {
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        setIPAddress(data.ip);
+      } catch (error) {
+        console.error('Error fetching IP address:', error);
+      }
+    };
+
+    fetchIPAddress();
+  }, [url]);
+
+  return <span className="AddressDisplay">{ipAddress}</span>;
+};
+
+export default AddressDisplay;
